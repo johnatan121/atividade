073e845ca79c061846f7b359692f58c4723b180c
johnatan121 <johnatan121@live.com>
2025-12-09 17:06:39 -0300

    Refactor cadastro and access logic in prova.py

--- prova.py
+++ prova.py
@@ -24,11 +24,11 @@ print("Lista cursos:",lista)"""
 
 #att 5 a nota
 nota1 = int(input("digite sua nota linguagen, codigos; "))
-nata2 = int(input("digiite sua nota matematica; "))
+nota2 = int(input("digiite sua nota matematica; "))
 nota3 = int(input("digite sua nota ciencias da natureza: "))
 nota4 = int(input("digite sua nota ciencias humanos; "))
 nota5 = int(input("digite sua nota da redação; "))
-soma= (nota1+nata2+nota3+nota4+nota5)
+soma= (nota1+nota2+nota3+nota4+nota5)
 media = (soma /5)
 print(f"sua nota {soma} sua media {media}")
 #att 6
@@ -38,12 +38,12 @@ elif media <=450 and nota5 ==0:
     print(f"aluno nao apto media inferio a 450, media aluno; {media} redaçao zerado, nota redação; {nota5} ")
 else: """
     #att 7
-valor_curso = "1000"
+valor_curso = 1000
 
 if media >= 701:
     print("bolsa 100%  ")
 elif media >=651:     
-    print(f"aluno ganhou 50% desconto; total com desconto: {valor_curso*0,5*48}")
+    print(f"aluno ganhou 50% desconto; total com desconto: {valor_curso*0.5*48}")
 elif media >= 601:
     print(f"aluno ganhou 40% desconto, total com desconto: {valor_curso*0.4*48}")  
 elif media >= 551:
@@ -52,4 +52,5 @@ elif media >= 451:
     print(f"aluno ganhou 30% desconto, total com desconto{valor_curso*0.30*48}")   
 else:
     print(f"aluno ganhou 20% desconto {valor_curso*0.20*48}")  
+
 
